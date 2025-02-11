588ec6615ac5e0ecac063e0bbc1f014744745a6f
Danny <dan@Optimus.local>
2025-02-11 21:33:21 +0000

    STORY_3 Added some additional docstrings

--- main/rental/CarRentalCompany.py
+++ main/rental/CarRentalCompany.py
@@ -14,9 +14,18 @@ class CarRentalCompany:
 
 
     def add_car(self, car: Car):
+        """
+        Adds a car to the inventory represented by self.cars
+        :param car: (Car) An instance of the Car object
+        """
         self.cars.append(car)
 
     def matching_cars(self, criteria: Criteria) -> list:
+        """
+        Returns a list of matching cars matching the supplied filter
+        :param criteria: (Criteria) An instance of the criteria class providing filter details
+        :return: (list) A list of available cars matching the criteria
+        """
         # This is a read operation so the lock really isn't going to do much within this context
         with self._rental_lock:
             # This method of filtering is extremely crude and should really not be used
@@ -41,7 +50,14 @@ class CarRentalCompany:
 
 
     def rent_car(self, renter: Renter, car: Car, date_period: DatePeriod) -> bool:
-        # This makes modifications to the inventory so this is where we need to have a lock to support multi threaded
+        """
+        Marks a car in the inventory as being rented
+        :param renter: (Renter) An instance of the Renter class denoting the person making the booking
+        :param car: (Car) An instance of the Car class denoting the car that is to be booked
+        :param date_period: (DatePeriod) An instance of DatePeriod that provides the date range to book the car for
+        :return: (bool) A boolean representing whether the booking was successful
+        """
+        # This makes modifications to the inventory so this is where we need to have a lock to support multi-threaded
         # access
         with self._rental_lock:
             if car.date_period:
--- main/rental/Renter.py
+++ main/rental/Renter.py
@@ -3,6 +3,13 @@ from datetime import date
 
 class Renter:
     def __init__(self, last_name, first_name, driving_license_number, date_of_birth):
+        """
+        A Renter class that captures details of the car renter
+        :param last_name: (str)  Last name of the renter
+        :param first_name:  (str) First name of the renter
+        :param driving_license_number: (str) The renter's driver's licence number
+        :param date_of_birth: (datetime.date) The renter's date of birth
+        """
         self.last_name = last_name
         self.first_name = first_name
         self.driving_license_number = driving_license_number
